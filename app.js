9f32610ee49b976df16e2cb5fa73966776f7a64a
SergeHuriaux <sergeoclockpromox@gmail.com>
2021-04-22 20:30:12 -0300

    CSS section Asymétrique - responsive

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ const menu = document.querySelector('.liste-nav');
 btnMenu.addEventListener('click', function () {
     menu.classList.toggle('active');
 })
+
+
+const allLinks = document.querySelectorAll('.item-nav');
+
+allLinks.forEach(function (item) {
+
+    item.addEventListener('click', function () {
+        menu.classList.toggle('active');
+    })
+
+})
